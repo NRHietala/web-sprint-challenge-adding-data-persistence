a739e2e73e6102c2afd189eeb99d8bfbd774a96c
Nathan Hietala <Nrhietala@gmail.com>
2021-02-19 08:24:28 -0600

    Server wired up, dependencies installed

--- api/server.js
+++ api/server.js
@@ -1,1 +1,13 @@
-// build your server here and require it from index.js
+const express = require("express");
+const server = express();
+server.use(express.json());
+
+server.get("/", (_, res) => {
+  res.send("Is your API running? You should go catch it! 🏃‍");
+});
+
+server.use("*", (_, res) => {
+  res.status(404).json({ message: "404: Resource not found" });
+});
+
+module.exports = server;
